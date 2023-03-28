6c5e363c38c8df903aa3d7918c510b02e8480392
Никита Зайцев <genius.zaytsev@gmail.com>
2023-03-28 20:08:47 +0500

    новые докстринги

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,3 +1,4 @@
+"""URL для приложения posts."""
 from django.urls import path
 
 from . import views
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,4 @@
+"""View-функции для приложения posts."""
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
